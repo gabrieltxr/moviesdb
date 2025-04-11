9793d3f6d9dbdbf842686ad5e223b64e4e4b4fcb
Gabriel <gabriel.txr.barros@gmail.com>
2025-04-11 11:52:46 -0300

    Att busca --- categorias

--- app.py
+++ app.py
@@ -19,15 +19,33 @@ def index():
 @app.route('/buscar', methods=['GET'])
 def buscar():
     termo = request.args.get('q', '')
+    coluna = request.args.get('coluna', 'titulo')  # pega o campo do <select>
+
+    # Mapeamento dos nomes do select para as colunas do banco
+    colunas_mapeadas = {
+        'titulo': 'movie_name',
+        'ano': 'movie_year',
+        'nota': 'imdb_score',
+        'genero': 'movie_genre',
+        'diretor': 'movie_director'
+    }
+
+    coluna_banco = colunas_mapeadas.get(coluna, 'movie_name')
 
     cur = conn.cursor()
-    print(f"Termo pesquisado: {termo}")
-    query = "SELECT movie_name, imdb_score, movie_year, movie_genre, movie_director FROM movies WHERE movie_name ILIKE %s"
+    print(f"Pesquisando por '{termo}' na coluna '{coluna_banco}'")
+
+    query = f"""
+        SELECT movie_name, imdb_score, movie_year, movie_genre, movie_director
+        FROM movies
+        WHERE {coluna_banco}::text ILIKE %s
+    """
     cur.execute(query, (f'%{termo}%',))
     resultados = cur.fetchall()
     cur.close()
 
     return render_template('index.html', resultados=resultados, termo=termo)
+
 
 
 if __name__ == '__main__':
